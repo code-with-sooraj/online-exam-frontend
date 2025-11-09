d467b54fa44a25caecd91f514d7d48298da09198
code-with-sooraj <sauravuyadav003@gmail.com>
2025-11-09 20:14:24 +0530

    url fixed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,7 +4,7 @@ let socket
 
 export function getSocket() {
   if (!socket) {
-    const url = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4001'
+    const url = 'https://online-exam-backend-xi.vercel.app/' || 'http://localhost:4001'
     socket = io(url, { transports: ['websocket'], withCredentials: false })
   }
   return socket
